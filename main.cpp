bdadcdd515a5c2446ef38047f4a4cdabc48bb02a
Ellie Moore <evcmoore@gmail.com>
2021-11-26 20:54:30 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -17,30 +17,22 @@ int main() {
     std::default_random_engine generator(clock());
     std::uniform_int_distribution<long> distribution(LONG_MIN, LONG_MAX);
     uint64_t y = 0;
-       double c = 0;
-        for (; y < 20; ++y) {
-            //if(!(y & 0x3)) std::cout << "#";
-            uint64_t x = 2000000;
-            //std::cout << x << '\n';
-            int *a = new int[x];
-            for (int *i = a; i < a + x; ++i) {
-                *i = distribution(generator) % x;
-            }
-            //for(int* i = a; i < a + x; i++)
-            //    std::cout << *i << ' ';
-            //std::cout << "\n\n";
-            //const int s = clock();
-            const int s = clock();
-            Arrays::uSort<int32_t>(a, x);
-            c += (double) (clock() - s) / CLOCKS_PER_SEC;
-            //std::cout << (double) (clock() - s) / CLOCKS_PER_SEC << '\n';
-            if (!test(a, x)) {
-                std::cout << "aw!";
-                return 0;
-            }
-            //std::cout << *(a) << '\n';
-            delete[] a;
+    double c = 0;
+    for (; y < 20; ++y) {
+        uint64_t x = 2000000;
+        int *a = new int[x];
+        for (int *i = a; i < a + x; ++i) {
+            *i = distribution(generator) % x;
         }
-        printf("%.10f\n", c);
+        const int s = clock();
+        Arrays::uSort<int32_t>(a, x);
+        c += (double) (clock() - s) / CLOCKS_PER_SEC;
+        if (!test(a, x)) {
+            std::cout << "aw!";
+            return 0;
+        }
+        delete[] a;
+    }
+    printf("%.10f\n", c);
     return 0;
 }
--- scr.sh
+++
@@ -1,6 +0,0 @@
-i=0
-while [[ i -lt 50 ]]
-do
-    ./erg
-    ((i++))
-done
